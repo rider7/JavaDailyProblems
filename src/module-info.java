d24acc47d2e68dee73e53a7b3fca256a9cb1e107
rider7 <brandon.rider@gmail.com>
2019-11-21 10:48:47 -0600

    Update module-info.java
    11/21-sort through a string returning a value. used: sort method, join method, and split method

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,18 @@
 module javaBasicProblems {
 }
+
+/*You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+
+
+public class SortAndStar {
+
+    public static String twoSort(String[] s) {
+        java.util.Arrays.sort(s);
+
+        return String.join("***",s[0].split(""));
+    }
+}*/
