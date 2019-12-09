605eca070c27d05772d6792f0afbf8b55aa1fa3f
rider7 <brandon.rider@gmail.com>
2019-12-09 13:47:29 -0600

    Update module-info.java
    endswith() method used to check if 2nd string is contained in the end of the 1st string

--- src/module-info.java
+++ src/module-info.java
@@ -1,13 +1,15 @@
 module javaBasicProblems {
 }
-
-/*You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
+//11-21-2019
+/*
+Description:
+ You will be given an vector of string(s). You must sort it alphabetically (case-sensitive!!) and then return the first value.
 
 The returned value must be a string, and have "***" between each of its letters.
 
 You should not remove or add elements from/to the array.
 
-
+Solution:
 public class SortAndStar {
 
     public static String twoSort(String[] s) {
@@ -15,4 +17,31 @@ public class SortAndStar {
 
         return String.join("***",s[0].split(""));
     }
-}*/
+}
+*/
+/*12-9-2019
+
+Description:
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+import java.lang.*;
+public class Kata {
+  public static boolean solution(String str, String ending) {
+ //check for 2nd string bigger than 1st string
+ if(str.length() < ending.length()){
+ return false;
+  }
+  //return true if 1st string ends with 2nd string using endsWith()method
+    return str.endsWith(ending);
+  }
+}
+
+Notes: used endsWith() method which checks whether the string ends with a specified suffix.This method returns a boolean value true or false.
+
+*/
